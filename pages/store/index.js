49555c4246e67897f6ac5ff8bfdbcb0be1d6b98c
Serhii Bilan <sergejbilan390@gmail.com>
2024-04-24 22:14:05 +0200

    22

--- pages/store/index.js
+++ pages/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import countReducer from './countSlice.js';
-import userReducer from './users.js'
 
 export default configureStore({
   reducer: {
     counts: countReducer,
-    user_data: userReducer,
   },
 });
